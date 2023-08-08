67c2df66bbe645b9fa4dff18e0dcbae7f7d9db41
pony0278 <steam2744@gmail.com>
2023-08-08 22:43:15 +0800

    完成作業一 改用 jQuery 完成JSON資料讀取並顯示在表格中的練習

---
+++ AJAX作業/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AJAX作業.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
